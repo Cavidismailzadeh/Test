c8047775a5375bde9e898fc9d2b91aede8a399d8
Cavidismailzadeh <119400508+Cavidismailzadeh@users.noreply.github.com>
2023-01-09 18:30:26 +0400

    Add files via upload

---
+++ clone/09.01.2023/Homework 09.01.2023(Practica)/Constants/ExceptionsMessages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_09._01._2023_Practica_.Constants
+{
+    internal class ExceptionsMessages
+    {
+        public const string InvalidLogin = "Username or password wrong";
+    }
+}
---
+++ clone/09.01.2023/Homework 09.01.2023(Practica)/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_09._01._2023_Practica_.Exceptions
+{
+    internal class InvalidLoginException : Exception
+    {
+        public InvalidLoginException(string message):base(message) { }
+    }
+}
